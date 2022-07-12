64abf14517b6423a9b56b0b146e8def41c5f9067
jankat11 <96008036+jankat11@users.noreply.github.com>
2022-07-13 01:17:28 +0300

    Update script.js

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -95,9 +95,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 alert("Login to see profile.")
             }
         } else if (icon.className == "commentIcon") {
-            if (header.dataset.profile == "AnonymousUser") {
-                window.location.href = `/login`
-            }
+
             let status = icon.parentElement.parentElement.dataset.comment
             comment(icon.parentElement.parentElement, icon, status)
         } else if ([...icon.classList].includes("postItem")) {
